4c7d14408bec584cfcc7d3eb92e58c2adb847a06
gyuwseong <69397352+gyuwseong@users.noreply.github.com>
2024-11-19 11:48:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 524 ms, Memory: 58624 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,35 @@
+import collections
+import sys
+
+n, m, r = map(int, sys.stdin.readline().split())
+graph = list([] for _ in range(n + 1))
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for g in graph:
+    g.sort(reverse=True)
+
+visited = [0] * (n + 1)
+count = 1
+
+
+def bfs(x):
+    q = collections.deque([x])
+    global count
+    visited[x] = count
+    count += 1
+    while q:
+        b = q.popleft()
+        for i in graph[b]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = count
+                count += 1
+
+
+bfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
